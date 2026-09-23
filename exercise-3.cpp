8f0907962d35f7b3b05e4cd89b02fe3859ce4843
whytex <102326124+wwhytex@users.noreply.github.com>
2026-09-23 23:40:23 +0100

    Create exercise-3.cpp

---
+++ exercise-3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a;
+	int b;
+
+	cout << "What's the first number?" << endl;
+	cin >> a;
+	cout << "What's the second number?" << endl;
+	cin >> b;
+	cout << "The result is: " << a + b;
+	return 0;
+}
