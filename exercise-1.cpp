e6b55a6eb17a8b53d18f4bf4a40e592ff308653a
whytex <102326124+wwhytex@users.noreply.github.com>
2026-09-23 22:07:12 +0100

    Add basic C++ program to greet user by name

---
+++ exercise-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+
+	cout << "Qual e o teu nome? " << endl;
+	cin >> name;
+	cout << "Ola " << name << " bem-vindo a programacao!";
+	return 0;
+
+}
