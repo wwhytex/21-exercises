7327e7c72f7754044c37369f31818fccc514e5b8
whytex <102326124+wwhytex@users.noreply.github.com>
2026-09-23 22:12:55 +0100

    Add basic user input program in C++

---
+++ exercise-2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string name;
+	int age;
+
+	cout << "Qual e a  tua idade? " << endl;
+	cin >> age;
+	cout << "Qual e o teu nome? " << endl;
+	cin >> name;
+	cout << "Ola sou o " << name << " e tenho anos " << age << endl;
+	return 0;
+}
