33d894c85469bb88990d2afa118bc2976b8d487e
whytex <102326124+wwhytex@users.noreply.github.com>
2026-09-24 00:03:21 +0100

    Add program to calculate area of a rectangle

---
+++ exercise-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a;
+	double b;
+
+	cout << "A = l x w" << endl;
+	cout << "Choose Lenght" << endl;
+	cin >> a;
+	cout << "Choose Widht" << endl;
+	cin >> b;
+	cout << "The area is: " << a * b << endl;
+	return 0;
+}
