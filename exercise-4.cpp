625fe88722ca45d283dfc1524efc7946be8aa063
whytex <102326124+wwhytex@users.noreply.github.com>
2026-09-23 23:50:52 +0100

    Implement basic calculator in exercise-4.cpp

---
+++ exercise-4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double a;
+	double b;
+	string input;
+
+	cout << "What's your first number?" << endl;
+	cin >> a;
+	cout << "What's your second number?" << endl;
+	cin >> b;
+	cout << "What's the mathematical operation? ( - , + , * , / ) are available." << endl;
+	cin >> input;
+
+	if (input == "-") {
+		cout << a - b << endl;
+	}
+	if (input == "+") {
+		cout << a + b << endl;
+	}
+	if (input == "*") {
+		cout << a * b << endl;
+	}
+	if (input == "/") {
+		cout << a / b << endl;
+	}
+	else {
+		cout << "Invalid operation" << endl;
+	}
+	return 0;
+}
